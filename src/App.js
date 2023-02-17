dfb11cb49793df4b33fd5fc5446a2ed0d9d14e09
joyumoekpo <joyumoekpo1@gmail.com>
2023-02-17 16:08:20 -0600

    Configured App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import './App.css';
 
-function App() {
+import { useContext } from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
+
+import Auth from './components/Auth/Auth';
+import Home from './components/Home/Home';
+import Header from './components/Header/Header';
+import Profile from './components/Profile/Profile';
+import AuthContext from './store/authContext';
+
+const App = () => {
+
+  const authCtx = useContext(AuthContext);
+
   return (
-    <div className="App"></div>
-  );
+    <div className='app'>
+      <Header/>
+        <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/auth' element={!authCtx.token ? <Auth/> : <Navigate to='/'/>}/>
+        <Route path='/profile' element={authCtx.token ?<Profile/> : <Navigate to='/auth'/>}/>
+        <Route path='*' element={<Navigate to='/'/>}/>
+        </Routes>
+    </div>
+  )
 }
 
-export default App;
+export default App
