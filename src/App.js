ba9628ff528c6f3fab7ee3e8d8ea2875c56ef266
joyumoekpo <joyumoekpo1@gmail.com>
2023-02-17 21:20:14 -0600

    Added Footer component under routes

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import Auth from './components/Auth/Auth';
 import Home from './components/Home/Home';
 import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 import Profile from './components/Profile/Profile';
 import AuthContext from './store/authContext';
 
@@ -22,6 +23,7 @@ const App = () => {
         <Route path='/profile' element={authCtx.token ?<Profile/> : <Navigate to='/auth'/>}/>
         <Route path='*' element={<Navigate to='/'/>}/>
         </Routes>
+        <Footer/>
     </div>
   )
 }
