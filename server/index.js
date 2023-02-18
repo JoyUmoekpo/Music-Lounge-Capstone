969c38be16daee2f42bd27f08bd7895e13ee684f
joyumoekpo <joyumoekpo1@gmail.com>
2023-02-18 01:20:40 -0600

    Configured server index.js for authentication

---
+++ server/index.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+
+const { PORT } = process.env;
+const { sequelize } = require("./util/database");
+const { User } = require("./models/user");
+const { register, login } = require("./controllers/auth");
+const { isAuthenticated } = require("./middleware/isAuthenticated");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.post("/register", register);
+app.post("/login", login);
+
+sequelize
+	.sync({ force: true })
+	.then(() => {
+		app.listen(PORT, () =>
+			console.log(
+				`Connection successful. The server is running on port ${PORT}`
+			)
+		);
+	})
+	.catch((err) => console.log(err));
