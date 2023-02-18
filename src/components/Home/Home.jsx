2b91639a202cec15ae9e7434e159ad6d00391880
joyumoekpo <joyumoekpo1@gmail.com>
2023-02-18 13:08:35 -0600

    Added background image

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import background from '../../assets/headphones.jpg'
+import styles from './Home.module.css'
 
 const Home = () => {
   return (
-    <div>Welcome to my React Capstone!</div>
+    <div 
+    style={{
+      backgroundImage: `url(${background})`,
+      height: '85vh',      
+    }}
+    className={styles.home_text}
+    >Welcome to my React Capstone!</div>
   )
 }
 
