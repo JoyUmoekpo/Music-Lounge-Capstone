6aee78010a53d678c8c3141a6b6111f5fcd855cf
joyumoekpo <joyumoekpo1@gmail.com>
2023-02-17 21:18:10 -0600

    Configured Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,60 @@
-import React from 'react'
-import styles from './Header.module.css';
+import React from "react";
+import { useContext } from "react";
+import { NavLink } from "react-router-dom";
+import AuthContext from "../../store/authContext";
+
+import styles from "./Header.module.css";
 
 const Header = () => {
-  return (
-    <div className={styles.header}>Header</div>
-  )
-}
+	const authCtx = useContext(AuthContext);
 
-export default Header
+	const styleActiveLink = ({ isActive }) => {
+		return {
+			color: isActive ? "#f57145" : "",
+		};
+	};
+
+	return (
+		<header>
+			<div>
+				<h2 className={styles.header}>Welcome</h2>
+			</div>
+			<nav>
+				{authCtx.token ? (
+					<ul className="main-nav">
+						<li>
+							<NavLink style={styleActiveLink} to="/">
+								Home
+							</NavLink>
+						</li>
+						<li>
+							<NavLink style={styleActiveLink} to="profile">
+								Profile
+							</NavLink>
+						</li>
+						<li>
+							<button className="logout-btn" onClick={() => authCtx.logout()}>
+								Logout
+							</button>
+						</li>
+					</ul>
+				) : (
+					<ul className="main-nav">
+						<li>
+							<NavLink style={styleActiveLink} to="/">
+								Home
+							</NavLink>
+						</li>
+						<li>
+							<NavLink style={styleActiveLink} to="/auth">
+								Login or Sign Up
+							</NavLink>
+						</li>
+					</ul>
+				)}
+			</nav>
+		</header>
+	);
+};
+
+export default Header;
