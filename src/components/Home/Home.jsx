0129571773ca1fabcd526244f43c04e5aa386991
joyumoekpo <joyumoekpo1@gmail.com>
2023-02-18 13:52:21 -0600

    Styled Homepage view

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,17 +1,27 @@
-import React from 'react'
-import background from '../../assets/headphones.jpg'
-import styles from './Home.module.css'
+import React from "react";
+import background from "../../assets/headphones.jpg";
+import styles from "./Home.module.css";
 
 const Home = () => {
-  return (
-    <div 
-    style={{
-      backgroundImage: `url(${background})`,
-      height: '85vh',      
-    }}
-    className={styles.home_text}
-    >Welcome to my React Capstone!</div>
-  )
-}
+	return (
+		<div
+			style={{
+				backgroundImage: `url(${background})`,
+				height: "85vh",
+			}}>
+			<div className={styles.home_text}>
+				<main className={styles.home_text_background}>
+					Welcome to my React App
+				</main>
+			</div>
+			<div className={styles.home_text}>
+				<summary className={styles.home_text_background}>
+					{" "}
+					[App Description]
+				</summary>
+			</div>
+		</div>
+	);
+};
 
-export default Home
+export default Home;
