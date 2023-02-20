8c1e30cc6197576888b898dbbcfeb7cef30405dd
joyumoekpo <joyumoekpo1@gmail.com>
2023-02-20 03:36:09 -0600

    Styled Favorites components

--- src/components/Favorites/Favorites.jsx
+++ src/components/Favorites/Favorites.jsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 import styles from "./Favorites.module.css";
 
 const Favorites = () => {
-	return <div>Favorites</div>;
+	return (
+		<Fragment>
+			<div className={styles.title}>[User's] Favorite Songs</div>
+			<div className={styles.favorites_container}>
+				<div className={styles.favorite}>Favorite Song</div>
+				<div className={styles.favorite}>Favorite Song</div>
+				<div className={styles.favorite}>Favorite Song</div>
+				<div className={styles.favorite}>Favorite Song</div>
+				<div className={styles.favorite}>Favorite Song</div>
+				<div className={styles.favorite}>Favorite Song</div>
+				<div className={styles.favorite}>Favorite Song</div>
+				<div className={styles.favorite}>Favorite Song</div>
+				<div className={styles.favorite}>Favorite Song</div>
+				<div className={styles.favorite}>Favorite Song</div>
+			</div>
+		</Fragment>
+	);
 };
 
 export default Favorites;
