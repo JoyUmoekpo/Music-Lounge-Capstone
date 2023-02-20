e1ef99ef70fd8499b0fb3716d9dc43118f5b4001
joyumoekpo <joyumoekpo1@gmail.com>
2023-02-20 04:24:07 -0600

    Added NavLink to Profile Page buttons

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,14 +1,20 @@
 import React, { Fragment } from "react";
+import { NavLink } from "react-router-dom";
 
 import styles from "./Profile.module.css";
 
 const Profile = () => {
 	return (
 		<Fragment>
+			<div className={styles.title}>[User's] Profile Page</div>
 			<div className={styles.profile_button_container}>
 				<button className={styles.profile_button}>Create a playlist</button>
-				<button className={styles.profile_button}>See your playlists</button>
-				<button className={styles.profile_button}>See your favorites</button>
+				<NavLink to="/playlist">
+					<button className={styles.profile_button}>See your playlists</button>
+				</NavLink>
+				<NavLink to="/favorites">
+					<button className={styles.profile_button}>See your favorites</button>
+				</NavLink>
 			</div>
 		</Fragment>
 	);
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+ import React from "react";
 import { useContext } from "react";
 import { NavLink } from "react-router-dom";
 import AuthContext from "../../store/authContext";
