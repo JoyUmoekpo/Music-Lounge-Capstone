bff7d0acd43b454012e0d7b2a6d0c1b9050bf5be
joyumoekpo <joyumoekpo1@gmail.com>
2023-02-20 04:41:48 -0600

    Added Footer message and styling

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import styles from './Footer.module.css'
+import React from "react";
+import styles from "./Footer.module.css";
 
 const Footer = () => {
-  return (
-    <div className={styles.footer}></div>
-  )
-}
+	return (
+		<div className={styles.footer}>
+			<div className={styles.footer_text}> © 2023 Music React Capstone</div>
+		</div>
+	);
+};
 
-export default Footer
+export default Footer;
