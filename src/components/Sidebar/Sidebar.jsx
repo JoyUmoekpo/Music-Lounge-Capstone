d4f0836c71436a1d9e03ad7fa7e8ca857e92cbf5
joyumoekpo <joyumoekpo1@gmail.com>
2023-02-19 23:18:33 -0600

    Added Context to Sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,10 +1,49 @@
-import React from 'react'
-import styles from './Sidebar.module.css'
+import React from "react";
+import { useContext } from "react";
+import { NavLink } from "react-router-dom";
+import AuthContext from "../../store/authContext";
+import styles from "./Sidebar.module.css";
 
 const Sidebar = () => {
-  return (
-    <div className={styles.sidebar}></div>
-  )
-}
+	const authCtx = useContext(AuthContext);
 
-export default Sidebar
+	const styleActiveLink = ({ isActive }) => {
+		return {
+			backgroundColor: isActive ? "#000" : "",
+		};
+	};
+
+	return (
+		<div className={styles.sidebar}>
+			<nav>
+				{authCtx.token ? (
+					<ul>
+						<li>
+							<NavLink style={styleActiveLink} to="profile">
+								Profile
+							</NavLink>
+						</li>
+						<li>
+							<NavLink style={styleActiveLink} to="playlist">
+								Playlists
+							</NavLink>
+						</li>
+						<li>
+							<NavLink style={styleActiveLink} to="favorites">
+								Favorites
+							</NavLink>
+						</li>
+					</ul>
+				) : (
+					<ul>
+						<li></li>
+						<li></li>
+						<li></li>
+					</ul>
+				)}
+			</nav>
+		</div>
+	);
+};
+
+export default Sidebar;
