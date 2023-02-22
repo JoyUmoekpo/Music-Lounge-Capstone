ddb9e641c841dd8d15fcfcd288952ff32d53206c
joyumoekpo <joyumoekpo1@gmail.com>
2023-02-21 22:01:15 -0600

    Removed unused logic

--- src/components/SongCard/SongCard.jsx
+++ src/components/SongCard/SongCard.jsx
@@ -4,14 +4,6 @@ import { useNavigate } from "react-router-dom";
 import styles from "./SongCard.module.css";
 
 const SongCard = ({song}) => {
-	const navigate = useNavigate();
-
-	// const handleClick = () => {
-	// 	navigate(`/search/${song}`);
-	// };
-
-	// onClick={handleClick}
-
 	return (
 		<div className={styles.card}>
 			<div>
