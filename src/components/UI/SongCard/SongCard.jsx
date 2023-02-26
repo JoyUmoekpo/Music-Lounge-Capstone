371ecd71ba002e7f8783ba7bbe7fa2484d93fba9
joyumoekpo <joyumoekpo1@gmail.com>
2023-02-26 05:30:12 -0600

    Styled search results

--- src/components/UI/SongCard/SongCard.jsx
+++ src/components/UI/SongCard/SongCard.jsx
@@ -1,22 +1,25 @@
 import React from "react";
-import ReactPlayer from 'react-player'
+import ReactPlayer from "react-player";
 import axios from "axios";
-
 
 import styles from "./SongCard.module.css";
 
-const SongCard = ({song}) => {
+const SongCard = ({ song }) => {
 	const url = "http://localhost:4040";
-	
-	const handleClick = (song_id) => {
-	axios.post(`${url}/favorite`, {song_id, user_id: localStorage.getItem('userId')})
-	.then((res) => {
-		console.log(res.data);
 
-	}).catch((err) => {
-		console.log(err.message);
-	})
-	}
+	const handleClick = (song_id) => {
+		axios
+			.post(`${url}/favorite`, {
+				song_id,
+				user_id: localStorage.getItem("userId"),
+			})
+			.then((res) => {
+				console.log(res.data);
+			})
+			.catch((err) => {
+				console.log(err.message);
+			});
+	};
 
 	return (
 		<div className={styles.card} key={song.id}>
@@ -28,19 +31,24 @@ const SongCard = ({song}) => {
 						className={styles.song_image}
 					/>
 				</div>
-				<h3 className={styles.song_text}>{song.title_short}</h3>
-				<h3 className={styles.song_text}>{song.artist.name}</h3>
+				<h3 className={styles.song_text}>Title: {song.title_short}</h3>
+				<h3 className={styles.song_text}>Artist: {song.artist.name}</h3>
 			</div>
-			<button className={styles.song_button} onClick={() => handleClick(song.id)}>Favorite Song</button>
-
-			<ReactPlayer
-        url={song.preview}
-        width="200px"
-        height="25px"
-        playing={false}
-        controls={true}
-				volume={0.3}
-      />
+			<button
+				className={styles.song_button}
+				onClick={() => handleClick(song.id)}>
+				Favorite Song
+			</button>
+			<div className={styles.react_player}>
+				<ReactPlayer
+					url={song.preview}
+					width="200px"
+					height="25px"
+					playing={false}
+					controls={true}
+					volume={0.3}
+				/>
+			</div>
 		</div>
 	);
 };
