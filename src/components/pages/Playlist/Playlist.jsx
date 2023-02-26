7adbcfae71915104b82d491430061b131da63f2c
joyumoekpo <joyumoekpo1@gmail.com>
2023-02-26 02:41:59 -0600

    Added consistent position styling across application pages

--- src/components/pages/Playlist/Playlist.jsx
+++ src/components/pages/Playlist/Playlist.jsx
@@ -10,10 +10,13 @@ const Playlist = () => {
 
 	return (
 		<Fragment>
+			<div className={styles.playlist_position}>
 			<div className={styles.title}>{authCtx.username}'s Playlists</div>
 			<button className={styles.playlist_button}>Create a playlist</button>
 			<div className={styles.playlists_container}>
 			</div>
+			</div>
+
 		</Fragment>
 	);
 };
--- src/components/pages/Recommendations/Recommendations.jsx
+++ src/components/pages/Recommendations/Recommendations.jsx
@@ -4,7 +4,7 @@ import styles from "./Recommendations.module.css";
 const Recommendations = () => {
 
   return (
-    <div className={styles.rec_container}>
+    <div className={styles.rec_position}>
       <div className={styles.title}>Recommendations</div>
     </div>
   );
