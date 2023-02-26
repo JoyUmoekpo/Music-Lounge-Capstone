d262658e12a85d44b8ab2f34cd62ed61b67bc2ab
joyumoekpo <joyumoekpo1@gmail.com>
2023-02-26 04:43:22 -0600

    Styled favorites page

--- src/components/pages/Favorites/FavoriteCard.jsx
+++ src/components/pages/Favorites/FavoriteCard.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 import styles from "./Favorites.module.css";
+
 const FavoriteCard = ({ song, deleteFavorites }) => {
 	const [favSongs, setfavSongs] = useState([]);
 
@@ -10,20 +11,31 @@ const FavoriteCard = ({ song, deleteFavorites }) => {
 
 	useEffect(() => {
 		axios
-		.get(baseUrl + "/track/" + song.song_id)
-		.then((res) => {
-			setfavSongs(res.data);
-		})
-		.catch((err) => {
-			console.log("Error in getAllFavorites");
-		});
+			.get(baseUrl + "/track/" + song.song_id)
+			.then((res) => {
+				setfavSongs(res.data);
+			})
+			.catch((err) => {
+				console.log("Error in getAllFavorites");
+			});
 	}, [song.song_id]);
 
 	return (
 		<div>
-			<div className={styles.favorite}>
-				{song.id} | {favSongs.id} | {favSongs.title} | {song.song_id}
-				<button onClick={() => deleteFavorites(song.id)}>Delete</button>
+			<div className={styles.favorites_container}>
+				<div className={styles.favorite}>
+					<span>{favSongs.id} </span>
+					<span> | </span>
+					<span>{favSongs.title_short} </span>
+					<span> | </span>
+					<span>{(+favSongs.duration / 60).toFixed(0)} minutes</span>
+					<span> |</span>
+					<button
+						onClick={() => deleteFavorites(song.id)}
+						className={styles.favorite_button}>
+						Delete
+					</button>
+				</div>
 			</div>
 		</div>
 	);
