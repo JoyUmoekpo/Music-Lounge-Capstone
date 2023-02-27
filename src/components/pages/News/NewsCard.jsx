86a0141bfcd3b2be6864c92337e6b8ac0c634c82
joyumoekpo <joyumoekpo1@gmail.com>
2023-02-26 23:54:55 -0600

    Styled News page

--- src/components/pages/News/NewsCard.jsx
+++ src/components/pages/News/NewsCard.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import styles from "./News.module.css";
 
-const NewsCard = () => {
+const NewsCard = ({ articles }) => {
   return (
-    <div>NewsCard</div>
-  )
-}
+    <div className={styles.news_card}>
+      <li>
+        <img
+          src={articles.urlToImage}
+          alt="Article"
+          className={styles.news_image}
+        />
+      <li className={styles.news_text}> <a href={articles.url} target="_blank">Link</a> </li>
+      </li>
+      <li className={styles.news_text}>Author: <br /> {articles.author}</li>
+      <li className={styles.news_text}>Title: <br />{articles.title}</li>
+    </div>
+  );
+};
 
-export default NewsCard
+export default NewsCard;
