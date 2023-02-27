e0fe6680657b0de9a38aa6d55785b80401521233
joyumoekpo <joyumoekpo1@gmail.com>
2023-02-27 03:19:38 -0600

    Successfully add lyrics get request

--- src/components/pages/Lyrics/Lyrics.jsx
+++ src/components/pages/Lyrics/Lyrics.jsx
@@ -1,19 +1,57 @@
-import React, { Fragment } from "react";
-import { BiPlayCircle, BiPauseCircle } from "react-icons/bi";
+import React, { Fragment, useState } from "react";
+import axios from "axios";
 
 import styles from "./Lyrics.module.css";
 
 const Lyrics = () => {
+	const [song, setSong] = useState("");
+	const [artist, setArtist] = useState("");
+
+	const lyricSearch = (e) => {
+		e.preventDefault();
+
+		const options = {
+			method: "GET",
+			url: "https://powerlyrics.p.rapidapi.com/getlyricsfromtitleandartist",
+			params: { title: `${song}`, artist: `${artist}` },
+			headers: {
+				"X-RapidAPI-Key": "41fcae3ee6mshf0e229e9e01ab0bp166308jsne9f57a0ecfda",
+				"X-RapidAPI-Host": "powerlyrics.p.rapidapi.com",
+			},
+		};
+
+		axios
+			.request(options)
+			.then(function (response) {
+				console.log(response.data);
+			})
+			.catch(function (error) {
+				console.error(error);
+			});
+	};
+
 	return (
 		<Fragment>
-			<div className={styles.player_position}>
-				<div className={styles.title}>Player</div>
-				<div className={styles.player_container}>
-					<div className={styles.player_icons}>
-						<BiPlayCircle size={50} />
-						<BiPauseCircle size={50} />
-					</div>
-					<div>Song information</div>
+			<div className={styles.lyrics_position}>
+				<div className={styles.title}>Search for Lyrics</div>
+				<div className={styles.lyric_form}>
+					<form onSubmit={lyricSearch}>
+						<input
+							type="text"
+							value={song}
+							onChange={(e) => setSong(e.target.value)}
+							placeholder="Enter a song title"
+							className={styles.lyric_input}
+						/>
+						<input
+							type="text"
+							value={artist}
+							onChange={(e) => setArtist(e.target.value)}
+							placeholder="Enter the artist's name"
+							className={styles.lyric_input}
+						/>
+						<button className={styles.lyric_button}>Submit</button>
+					</form>
 				</div>
 			</div>
 		</Fragment>
