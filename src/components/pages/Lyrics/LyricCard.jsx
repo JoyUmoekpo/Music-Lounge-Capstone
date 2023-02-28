cddc6213c8a5bcd6bbc7b7be99920e2eca549397
joyumoekpo <joyumoekpo1@gmail.com>
2023-02-28 01:13:50 -0600

    Styled LyricCard

--- src/components/pages/Lyrics/LyricCard.jsx
+++ src/components/pages/Lyrics/LyricCard.jsx
@@ -3,25 +3,27 @@ import React from "react";
 import styles from "./Lyrics.module.css";
 
 const LyricCard = ({ lyrics }) => {
-  const thumbnail = lyrics.thumbnail.genius;
-  const title = lyrics.title;
-  const author = lyrics.author;
-  const lyricText = lyrics.lyrics;
+	const thumbnail = lyrics.thumbnail.genius;
+	const title = lyrics.title;
+	const author = lyrics.author;
+	const lyricText = lyrics.lyrics;
+	const link = lyrics.links.genius;
 
-  return (
-    <div className={styles.lyrics_card}>
-      <div className={styles.card_header}>
-        <img src={thumbnail} alt="thumbnail" className={styles.thumbnail} />
-        <div className={styles.title_author}>
-          <h2 className={styles.title}>{title}</h2>
-          <h3 className={styles.author}>{author}</h3>
-        </div>
-      </div>
-      <div className={styles.card_body}>
-        <p className={styles.lyrics_text}>{lyricText}</p>
-      </div>
-    </div>
-  );
+	return (
+		<div className={styles.lyrics_card}>
+			<div className={styles.card_header}>
+				<img src={thumbnail} alt="thumbnail" className={styles.thumbnail} />
+				<div className={styles.title_author}>
+					<h2>{title}</h2>
+					<h3>{author}</h3>
+				</div>
+			</div>
+			<div className={styles.card_body}>
+				<p>{lyricText}</p>
+				<a href={link} rel="noreferrer" target="_blank">Link to Genius Lyrics</a>
+			</div>
+		</div>
+	);
 };
 
 export default LyricCard;
--- src/components/pages/Lyrics/Lyrics.jsx
+++ src/components/pages/Lyrics/Lyrics.jsx
@@ -5,52 +5,51 @@ import axios from "axios";
 import styles from "./Lyrics.module.css";
 
 const Lyrics = () => {
+	const [search, setSearch] = useState("");
+	const [title, setTitle] = useState([]);
 
-  const [search, setSearch] = useState("");
-  const [title, setTitle] = useState([]);
+	const url = "http://localhost:4040";
 
-  const url = "http://localhost:4040";
+	const lyricSearch = (e) => {
+		e.preventDefault();
 
-  const lyricSearch = (e) => {
-    e.preventDefault();
+		axios
+			.get(`${url}/lyrics/${search}`)
+			.then((res) => {
+				console.log(res.data);
+				setTitle([res.data]);
+				setSearch("");
+			})
+			.catch((err) => {
+				console.log(err);
+				console.log("Error in lyricSearch");
+			});
+	};
 
-    axios
-      .get(`${url}/lyrics/${search}`)
-      .then((res) => {
-        console.log(res.data);
-        setTitle([res.data]);
-        setSearch("");
-      })
-      .catch((err) => {
-        console.log(err);
-        console.log("Error in lyricSearch");
-      });
-  };
+	const mappedLyrics = title.map((lyric) => {
+		return <LyricCard lyrics={lyric} />;
+	});
 
-  const mappedLyrics = title.map((lyric) => {
-    return <LyricCard lyrics={lyric} />;
-  });
-
-  return (
-    <Fragment>
-      <div className={styles.lyrics_position}>
-        <div className={styles.title}>Search for Lyrics</div>
-        <div className={styles.lyric_form}>
-          <form onSubmit={lyricSearch}>
-            <input
-              type="text"
-              value={search}
-              onChange={(e) => setSearch(e.target.value)}
-              placeholder="Enter a song title and artist"
-              className={styles.lyric_input}
-            />
-            <button className={styles.lyric_button}>Submit</button>
-          </form>
-        </div>
-        {mappedLyrics}
-      </div>
-    </Fragment>
-  );
+	return (
+		<Fragment>
+			<div className={styles.lyrics_position}>
+				<h1 className={styles.title}>Search for Lyrics</h1>
+				<div className={styles.lyric_form}>
+					<form onSubmit={lyricSearch}>
+						<input
+							type="text"
+							value={search}
+							onChange={(e) => setSearch(e.target.value)}
+							placeholder="Enter a song title and artist"
+							className={styles.lyric_input}
+						/>
+						<button className={styles.lyric_button}>Submit</button>
+					</form>
+				</div>
+				<div className={styles.lyrics_container}>{mappedLyrics}</div>
+			</div>
+		</Fragment>
+	);
 };
 
 export default Lyrics;
